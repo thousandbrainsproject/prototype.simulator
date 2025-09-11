e1de86c79a9e2cfbc9b6f83fafb5a3cb328b5aa8
codeallthethingz <wwarren@thousandbrains.org>
2025-09-11 11:45:30 +0100

    removed buf stuff, server with reflection

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ import logging
 from concurrent import futures
 
 import grpc
+from grpc_reflection.v1alpha import reflection
 
 import tbp.simulator.protocol.v1.protocol_pb2 as protocol_pb2
 import tbp.simulator.protocol.v1.protocol_pb2_grpc as protocol_pb2_grpc
@@ -23,11 +24,18 @@ def serve():
     protocol_pb2_grpc.add_SimulatorServiceServicer_to_server(
         SimulatorServiceServicer(), server
     )
+    SERVICE_NAMES = (
+        protocol_pb2.DESCRIPTOR.services_by_name["SimulatorService"].full_name,
+        reflection.SERVICE_NAME,
+    )
+    reflection.enable_server_reflection(SERVICE_NAMES, server)
     server.add_insecure_port("[::]:50051")
     server.start()
     server.wait_for_termination()
 
 
 if __name__ == "__main__":
-    logging.basicConfig()
+    logging.basicConfig(level=logging.INFO)
+    logger = logging.getLogger(__name__)
+    logger.info("Starting server")
     serve()
